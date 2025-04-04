cd5e58497f9cb4c83653fe3980ef232e84a18e73
vivek792 <55658429+vivek792@users.noreply.github.com>
2025-04-04 14:34:00 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-# Create your models here.
-class Data(models.Model):
-    Name = models.CharField(max_length=100)
-    Age = models.CharField(max_length=100)
-    Place = models.CharField(max_length=100)
+# Create your mode
