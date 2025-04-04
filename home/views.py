89839f9f04dd5eec6100f4a22cef3fa6354b14ad
vivek792 <55658429+vivek792@users.noreply.github.com>
2025-04-04 14:33:19 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Data
-from .serializers import DataModels
+# from .models import Data
+# from .serializers import DataModels
 
 
 # Create your views here.
@@ -11,33 +11,22 @@ from .serializers import DataModels
 @api_view(['GET'])
 def getting_data(request):
     if request.method == 'GET':
-        data = Data.objects.all()
-        serializers = DataModels(data,many=True)
-        return Response(serializers.data)
+        print("zgot the statement")
     
 
 @api_view(['POST'])
 def sending_data(request):
     if request.method == 'POST':
-        serializers = DataModels(data = request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data,status=status.HTTP_200_OK)
+        print("zgot the statement")
         
 
 @api_view(['PUT'])
 def updating(request,id):
     if request.method == 'PUT':
-        data = Data.objects.get(id=id)
-        serializers = DataModels(data,request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data,status=status.HTTP_202_ACCEPTED)
+        print("zgot the statement")
         
 @api_view(['DELETE'])
 def dele(request):
     if request.method == 'DELETE':
-        data = Data.objects.all()
-        data.delete()
-        return Response(status=status.HTTP_200_OK)
+        print("zgot the statement")
     
